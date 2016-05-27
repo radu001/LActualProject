412321ad7ceedb02f91d8fd923cdb3799ca6e443
radu001 <zmeuradu@mail.ru>
2016-05-27 12:22:56 +0300

    Implemented database lock mechanism

---
+++ CloudBackupL/DatabaseConnection.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+using System;
+using System.Configuration;
+
+namespace CloudBackupL
+{
+    class DatabaseConnection
+    {
+        private static String connString = ConfigurationManager.AppSettings["DbSQLite"];
+        private static SQLite.SQLiteConnection _connection;
+        public static SQLite.SQLiteConnection connection
+        {
+            get
+            {
+                if (_connection == null)
+                {
+                    _connection = new SQLiteConnection(connString, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create | SQLiteOpenFlags.FullMutex, true);
+                }
+                return _connection;
+            }
+        }
+
+    }
+}
--- CloudBackupL/DatabaseService.cs
+++ CloudBackupL/DatabaseService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using SQLite;
 using System.IO;
 
 namespace CloudBackupL
@@ -9,236 +8,199 @@ namespace CloudBackupL
     class DatabaseService
     {
         String connString = ConfigurationManager.AppSettings["DbSQLite"];
+        private static bool checkDBExists = true;
 
         public DatabaseService()
         {
-            if (!File.Exists(connString))
+            if (checkDBExists && !File.Exists(connString))
             {
-                using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+                checkDBExists = false;
+                lock (DatabaseConnection.connection)
                 {
-                    conn.CreateTable<Cloud>();
-                    conn.CreateTable<BackupPlan>();
-                    conn.CreateTable<Backup>();
+                    DatabaseConnection.connection.CreateTable<Cloud>();
+                    DatabaseConnection.connection.CreateTable<BackupPlan>();
+                    DatabaseConnection.connection.CreateTable<Backup>();
                 }
             }
-            
         }
 
         public int InsertBackupPlan(BackupPlan backupPlan)
         {
-            int key;
-            using(SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                key = conn.Insert(backupPlan);
+                return DatabaseConnection.connection.Insert(backupPlan);
             }
-            return key;
         }
 
         public int UpdateBackupPlan(BackupPlan backupPlan)
         {
-            int key;
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                key = conn.Update(backupPlan);
+                return DatabaseConnection.connection.Update(backupPlan);
             }
-            return key;
         }
 
         public int UpdateCloud(Cloud cloud)
         {
-            int key;
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                key = conn.Update(cloud);
+                return DatabaseConnection.connection.Update(cloud);
             }
-            return key;
         }
 
         public int InsertCloud(Cloud cloud)
         {
-            int key;
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                key = conn.Insert(cloud);
+                return DatabaseConnection.connection.Insert(cloud);
             }
-
-            return key;
         }
 
         public void InsertBackup(Backup backup)
         {
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                conn.Insert(backup);
+                DatabaseConnection.connection.Insert(backup);
+                BackupPlan backupPlan = GetBackupPlan(backup.backupPlanId);
+                backupPlan.lastExecution = backup.date;
+                backupPlan.currentStatus = "Updated";
+                backupPlan.lastResult = true;
+                backupPlan.lastBackupDuration = backup.runTime;
+                DatabaseConnection.connection.Update(backupPlan);
             }
-
-            BackupPlan backupPlan = GetBackupPlan(backup.backupPlanId);
-            backupPlan.lastExecution = backup.date;
-            backupPlan.currentStatus = "Updated";
-            backupPlan.lastResult = true;
-            backupPlan.lastBackupDuration = backup.runTime;
-
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
-            {
-                conn.Update(backupPlan);
-            }
-
         }
 
         public List<Cloud> GetAllClouds()
         {
-            List<Cloud> result;
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                result = conn.Query<Cloud>("select * from cloud");
+                return DatabaseConnection.connection.Query<Cloud>("select * from cloud");
             }
-            return result;
         }
 
         public List<BackupPlan> GetAllPlans()
         {
-            List<BackupPlan> result;
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                result = conn.Query<BackupPlan>("select * from BackupPlan");
+                return DatabaseConnection.connection.Query<BackupPlan>("select * from BackupPlan");
             }
-            return result;
         }
 
         public bool CheckPlanName(String name)
         {
-            int count = 1;
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                count = conn.Query<Cloud>("select * from cloud where name = ?", name).Count;
+                return DatabaseConnection.connection.Query<Cloud>("select * from cloud where name = ?", name).Count == 0 ? true : false;
             }
-            return count == 0 ? true : false;
         }
 
         public void DeletePlan(int id)
         {
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                conn.Delete<BackupPlan>(id);
-                conn.Query<Backup>("delete from Backup where backupPlanId = ?", id);
+                DatabaseConnection.connection.Delete<BackupPlan>(id);
+                DatabaseConnection.connection.Query<Backup>("delete from Backup where backupPlanId = ?", id);
             }
         }
 
         public BackupPlan GetBackupPlan(int id)
         {
-            BackupPlan plan = null;
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                plan = conn.Get<BackupPlan>(id);
+                return DatabaseConnection.connection.Get<BackupPlan>(id);
             }
-            return plan;
         }
 
         public Cloud GetCloudByName(String name)
         {
-            List<Cloud> cloud;
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                cloud = conn.Query<Cloud>("select * from cloud where name = ?", name);
+                return DatabaseConnection.connection.Query<Cloud>("select * from cloud where name = ?", name)[0];
             }
-            return cloud[0];
         }
 
         public bool IsCloudAlreadyInsered(string id)
         {
-            int count = 0;
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                count = conn.Query<Cloud>("select * from cloud where id = ?", id).Count;
+                return DatabaseConnection.connection.Query<Cloud>("select * from cloud where id = ?", id).Count > 0 ? true : false;
             }
-            return count > 0 ? true : false;
         }
 
         public bool CanDeleteCloud(string id)
         {
-            int count = 0;
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                count = conn.Query<Cloud>("select * from BackupPlan where cloudId = ?", id).Count;
+                return DatabaseConnection.connection.Query<Cloud>("select * from BackupPlan where cloudId = ?", id).Count > 0 ? false : true;
             }
-            return count > 0 ? false : true;
         }
 
         public void DeleteCloud(string id)
         {
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                conn.Delete<Cloud>(id);
+                DatabaseConnection.connection.Delete<Cloud>(id);
             }
         }
 
         public void DeleteBackup(int id)
         {
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                conn.Delete<Backup>(id);
+                DatabaseConnection.connection.Delete<Backup>(id);
             }
         }
 
         public List<Backup> GetBackCloudBackups(string cloudId)
         {
-            List<Backup> result;
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                result = conn.Query<Backup>("select * from Backup where cloudId = ?", cloudId);
+                return DatabaseConnection.connection.Query<Backup>("select * from Backup where cloudId = ?", cloudId);
             }
-            return result;
         }
 
         public Backup GetBackup(int backupId)
         {
-            Backup result;
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                result = conn.Get<Backup>(backupId);
+                return DatabaseConnection.connection.Get<Backup>(backupId);
             }
-            return result;
         }
 
         public Backup GetLastBackup(int planId)
         {
-            List<Backup> result;
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                result = conn.Query<Backup>("select * from Backup where backupPlanId = ? order by id desc limit 1", planId);
+                List<Backup> result;
+                result = DatabaseConnection.connection.Query<Backup>("select * from Backup where backupPlanId = ? order by id desc limit 1", planId);
+                return result.Count > 0 ? result[0] : null;
             }
-            return result.Count > 0 ? result[0] : null;
         }
 
         public Cloud GetCloud(string cloudId)
         {
-            Cloud result;
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                result = conn.Get<Cloud>(cloudId);
+                return DatabaseConnection.connection.Get<Cloud>(cloudId);
             }
-            return result;
         }
 
         public List<Backup> GetBackupsByPlanId(int planId)
         {
-            List<Backup> result;
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                result = conn.Query<Backup>("select * from Backup where backupPlanId = ?", planId);
+                return DatabaseConnection.connection.Query<Backup>("select * from Backup where backupPlanId = ?", planId);
             }
-            return result;
         }
 
         public Cloud GetCloudByToken(string accessToken)
         {
-            List<Cloud> clouds;
-            using (SQLiteConnection conn = new SQLiteConnection(connString, true))
+            lock (DatabaseConnection.connection)
             {
-                clouds = conn.Query<Cloud>("select * from Cloud where token = ?", accessToken);
+                List<Cloud> clouds;
+                clouds = DatabaseConnection.connection.Query<Cloud>("select * from Cloud where token = ?", accessToken);
+                return clouds.Count == 1 ? clouds[0] : null;
             }
-            return clouds.Count == 1 ? clouds[0] : null;
         }
-
     }
 }
